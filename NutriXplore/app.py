48ee065d303bfc93f5efc61fa1192599fb8f274b
Sumit27190 <sumitkapse27190@gmail.com>
2025-04-25 20:40:27 +0530

    Update app.py

--- NutriXplore/app.py
+++ NutriXplore/app.py
@@ -5,6 +5,6 @@ st.set_page_config(page_title="NutriXplore", page_icon="🍽️", layout="wide")
 st.markdown("<h1 style='text-align: center; color: #D32F2F;'>🍽️ Welcome to NutriXplore</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>An AI-powered tool to analyze Indian food nutrition and health suitability.</p>", unsafe_allow_html=True)
 
-st.image("assets/cover.jpg", use_column_width=True)
+st.image("assets/cover.jpeg", use_column_width=True)
 
 st.info("Navigate from the sidebar to explore!")
